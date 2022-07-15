0847bb029133c552e2acc421d90764637850ecba
Tom Harris <thomas.e.harris@gmail.com>
2022-07-15 16:54:13 -0400

    ran graphql generator

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 # typed: strict
 Rails.application.routes.draw do
+  post "/graphql", to: "graphql#execute"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
